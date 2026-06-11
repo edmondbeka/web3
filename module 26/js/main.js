40777fcca3a93659eca92c447d83feafcec4106e
edmondbeka <edmondbeka11@gmail.com>
2026-06-11 17:48:17 +0200

    commit

---
+++ module 26/js/main.js
@@ -0,0 +1,52 @@
+console.log("hello from edmond")
+
+var x=5;
+
+
+var y=28+43;
+console.log(y);
+
+var b=6%2;
+console.log(b);
+
+var c=99;
+c++;
+console.log(c);
+
+var c=99;
+c--;
+console.log(c);
+
+var a=4;
+var p=a**2;
+console.log(p);
+
+var b=6;
+b*=2;
+console.log(b);
+
+var b=6;
+var a=6;
+console.log(a==b);
+
+var b=7;
+var a="7";
+console.log(a===b);
+
+
+var b=6;
+var a=7;
+console.log(a>b);
+
+var b=6;
+var a=74;
+console.log(a<b);
+
+var b=6;
+var a=6;
+console.log(a!=b);
+
+var name="edmond";
+var lastname="beka";
+var fullname= name +"" +lastname;
+console.log(fullname)
